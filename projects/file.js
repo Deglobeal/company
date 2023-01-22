eeb20319a10682010cd0563f6a3af9dc69fcef32
Ugwu Gerard <kachimaxy1@gmail.com>
2023-01-22 10:09:11 +0100

    NEW WERK

---
+++ projects/file.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("btn");
+let output = document.getElementById("outputtext");
+
+let Number = [Math.floor(Math.random() * 100)]
+
+btn.addEventListener ('click', function() {
+         let input = document.getElementById("userinput").value;
+        if (input == Number) {
+            output.innerHTML == 'You gessed right, Your number was ${Number}'
+        } else if (input < Number) {
+            output.innerHTML = "You gessed to low"
+        };
+        if (input > Number) {
+            output.innerHTML = "You gessed too high"
+        };
+    }); 
